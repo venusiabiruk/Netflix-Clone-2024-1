b161c3f023deee1583744819dd0cb6896793903f
Venusia Biruk <venusiabiruk16@gmail.com>
2024-11-25 23:48:03 +0300

    banner

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import Banner from "../../components/Banner/Banner";
 import Footer from '../../components/Footer/Footer'
+
 
 const Home = () => {
   return (
     <>
-          <Header />
-          <Footer />
+      <Header />
+      <Banner />
+      <Footer />
     </>
-  )
+  );
 }
 
 export default Home
--- netflix-clone/src/utils/axios.js
+++ netflix-clone/src/utils/axios.js
@@ -1,4 +1,5 @@
-import React from 'react'
+// import React from 'react'
+import axios from "axios";
 const instance = axios.create({
     baseURL: "http://api.themoviedb.org/3",
 });
